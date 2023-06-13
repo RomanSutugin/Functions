488f45df971005e2388c9e0c277e52d4e53afa83
RomanSutugin <sutuginroman15@gmail.com>
2023-06-13 15:52:27 +0300

    Arrays functions

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
-void FillRand(int arr, const int n=10);
-int Print(int arr, const int n = 10);
+//void FillRand(int arr[], const int n = 10);
+//int Print(int arr[], const int n = 10);
+int Sum(const int arr[], const int n);
+double Avg(const int arr[], const int n);
+int minValue(const int arr[], const int n);
 
 
 void main()
@@ -9,28 +12,45 @@ void main()
 	setlocale(LC_ALL, "Rus");
 	const int n = 10;
 	int arr[n];
+	cout << "Сумма элементов массива: " << Sum(arr, n);
+	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n);
 	/*FillRand(arr, n);
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << endl;
-	}*/
-	int Print(arr, n);
+	}
+	int Print(arr[], n);*/
 }
 
-int Print()
+/*int Print()
 {
+	const int n = 10;
 	for (int i = 0; i < n; i++)
 	{
-		int arr[n] = rand();
+		int arr[i] = rand();
 		return arr[i];
 	}
 }
-/*void FillRand(int arr, int n)
+void FillRand(int arr[], int n)
 {
 	for (int i = 1; i <n; i++)
 	{
+		const int n = 10;
 		int arr[n];
 		arr[i] = rand();
 	}
 	return	0;
+}*/
+int Sum(const int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(const int arr[], const int n)
+{
+	return (double)Sum(arr,n) / n;
 }
