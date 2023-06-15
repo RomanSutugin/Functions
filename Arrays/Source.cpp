3ff92cbb3990317119095c4892eac8a6d2ba9d01
RomanSutugin <sutuginroman15@gmail.com>
2023-06-15 13:28:49 +0300

    Function overload DONE

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 using namespace std;
-//void FillRand(int arr[], const int n = 10);
-//int Print(int arr[], const int n = 10);
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(char arr[], const int n);
+
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(char arr[], const int n);
+
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+void Sort(char arr[], const int n);
+
 int Sum(const int arr[], const int n);
+double Sum(const double arr[], const int n);
+char Sum(const char arr[], const int n);
+
 double Avg(const int arr[], const int n);
-int minValue(const int arr[], const int n);
+char Avg(const char arr[], const int n);
+
+int minValue(const int arr[], const int n); 
+double minValue(const double arr[], const int n);
+char minValue(const char arr[], const int n);
+
+int maxValue(const int arr[], const int n);
+double maxValue(const double arr[], const int n);
+char maxValue(const char arr[], const int n);
 
 
 void main()
@@ -12,35 +33,112 @@ void main()
 	setlocale(LC_ALL, "Rus");
 	const int n = 10;
 	int arr[n];
-	cout << "Сумма элементов массива: " << Sum(arr, n);
-	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n);
-	/*FillRand(arr, n);
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << endl;
-	}
-	int Print(arr[], n);*/
+
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << minValue(arr, n) << endl;
+	cout << "Максимальное значение в массиве: " << maxValue(arr, n) << endl;
+	Sort(arr, n);
 }
 
-/*int Print()
-{
-	const int n = 10;
-	for (int i = 0; i < n; i++)
-	{
-		int arr[i] = rand();
-		return arr[i];
-	}
-}
 void FillRand(int arr[], int n)
 {
-	for (int i = 1; i <n; i++)
+	for (int i = 0; i <n; i++)
 	{
-		const int n = 10;
-		int arr[n];
-		arr[i] = rand();
+		arr[i] = rand() % 100;
+	};
+
+}
+void FillRand(double arr[], int n)
+{
+	for (int i = 0; i <n; i++)
+	{
+		arr[i] = rand() % 100;
+	};
+}
+
+void FillRand(char arr[], int n)
+{
+	for (int i = 0; i <n; i++)
+	{
+		arr[i] = rand() % 100;
+	};
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
 	}
-	return	0;
-}*/
+	cout << endl;
+}
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(char arr[], const int n)
+{
+	for (char i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
+void Sort(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(char arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
 int Sum(const int arr[], const int n)
 {
 	int sum = 0;
@@ -50,7 +148,89 @@ int Sum(const int arr[], const int n)
 	}
 	return sum;
 }
+
+double Sum(const double arr[], const int n)
+{
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+char Sum(const char arr[], const int n)
+{
+	char sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
 double Avg(const int arr[], const int n)
 {
 	return (double)Sum(arr,n) / n;
+}
+char Avg(const char arr[], const int n)
+{
+	return (char)Sum(arr,n) / n;
+}
+
+int minValue(const int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+
+double minValue(const double arr[], const int n)
+{
+	double min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+char minValue(const char arr[], const int n)
+{
+	char min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+
+int maxValue(const int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+
+double maxValue(const double arr[], const int n)
+{
+	double max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+char maxValue(const char arr[], const int n)
+{
+	char max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
 }
