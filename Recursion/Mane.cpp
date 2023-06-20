0e7d45fc5c1feb325fc833d732c7da43d888d7dc
RomanSutugin <sutuginroman15@gmail.com>
2023-06-20 11:49:38 +0300

    Power and Factorial done

--- Recursion/Mane.cpp
+++ Recursion/Mane.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 void elevator(int floor);
+int Factorial(int number);
+int Power(int n, int power);
+void Fibonacci_1(int a, int b, int limit);
 
 
 void main()
@@ -12,11 +15,29 @@ void main()
 	//cout << "Для выхода нажмите Escape, для продолжения нажмите любую кнопку" << endl;
 	//if(_getch()!= 27)main();
 
-	int floor;
+	/*int floor;
 	cout << "Введите номер этажа "; cin >> floor;
-	elevator(floor);
+	elevator(floor);*/
+
+	int number;
+	cout << "Введите число: "; cin >> number;
+	cout << "Факториал числа " << number << "=" << Factorial(number) << endl;
+
+	int n;
+	int power;
+	cout << "Введите число: "; cin >> n;
+	cout << "Введите степень: "; cin >> power;
+	int result = Power(n, power);
+	cout << "Число " << n << " в степени " << power << " = " << Power(n, power) << endl;
+
+	/*int limit;
+	cout << "Введите предел ряда Фибоначчи: "; cin >> limit;
+	cout << "Ряд Фибоначчи до " << limit << ":" << endl; 
+	Fibonacci_1(0, 1, limit);*/
+
 }
 
+#ifdef DEBUG
 void elevator(int floor)
 {
 	if (floor == 0)
@@ -25,6 +46,47 @@ void elevator(int floor)
 		return;
 	}
 	cout << "Вы на " << floor << " этаже\n";
-	elevator(floor-1);
+	elevator(floor - 1);
 	cout << "Вы на " << floor << " этаже\n";
 }
+#endif // DEBUG
+
+
+int Factorial(int number)
+{
+	if (number == 0) 
+		return 1;
+	else
+	{
+		return number * Factorial(number - 1);
+	}
+}
+
+int Power(int n, int power)
+{
+	int result = n;
+	if (power == 1)
+	{
+		return n;
+	}
+	else if (power == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * Power (n, power -1);
+	}
+}
+
+/*void Fibonacci_1(int a, int b, int limit)
+{
+	if (limit == a)
+	{
+		return;
+	}
+	cout << a << endl;
+	Fibonacci_1(b, a+b, limit);*/
+}
+
+
