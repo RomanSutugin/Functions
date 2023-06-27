24f36c0b11edcfcc11131079adf56ffb20244a3e
RomanSutugin <sutuginroman15@gmail.com>
2023-06-27 12:19:08 +0300

    reload DONE

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
+void Move(int key);
+
 
 #define VERTICAL_OFFSET		 "\n\n\n\n\n"
 #define HORIZONTAL_OFFSET	 "\t\t\t\t\t"
@@ -12,7 +15,7 @@ void main()
 	const int n = 9;
 	char field[n] = { /*1,2,3,4,5,6,7,8,9*/ };
 	PrintField(field, n);
-
+	Move(key);
 }
 
 void PrintField(char field[], const int n)
@@ -21,7 +24,7 @@ void PrintField(char field[], const int n)
 	for (int i = 6; i >= 0; i -= 3)
 	{
 		cout << HORIZONTAL_OFFSET;
-		cout << " ";
+		cout << "   ";
 		for (int j = 0; j < 3; j++)
 		{
 			cout << /*(int)*/field[i + j];
@@ -32,4 +35,12 @@ void PrintField(char field[], const int n)
 		if(i>0) cout << "--- --- ---";
 		cout << endl;
 	}
+}
+
+void Move(int key)
+{
+	key = _getch()
+		if (key == 1)
+			char field[n] = { char(88), , , , , , , , };
+	void Move(key)
 }
